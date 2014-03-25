b3f49db22428219cc7b0cb7030f30fa1f6dda9ab
Jeff Douglas <jeff@jeffdouglas.com>
2014-03-25 09:34:48 -0400

    finished up menu selection

--- app.js
+++ app.js
@@ -28,21 +28,46 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+var menus = function(selected) {
+  var default_menus =  {
+    home: 'menu-item menu-item-type-post_type menu-item-object-page menu-item-184',
+    challenges: 'menu-item menu-item-type-post_type menu-item-object-page menu-item-189',
+    leaderboard: 'menu-item menu-item-type-post_type menu-item-object-page menu-item-190'
+  }
+  if (selected === '/') {
+    default_menus.home = 'menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-12 current_page_item menu-item-184';
+  } else if (selected === '/challenges') {
+    default_menus.challenges = 'menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-29';
+  } else if (selected === '/leaderboard') {
+    default_menus.leaderboard = 'menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-111 current_page_item menu-item-190';
+  }
+  return default_menus;
+}
+
 app.get('/', function(req, res) {
   res.render('index', 
-    { title: 'Home', banner: 'banner_about.png' }
+    { title: 'Home', 
+      banner: 'banner_about.png',
+      menu: menus(req.route.path)
+    }
   )
 });
 
 app.get('/challenges', function(req, res) {
   res.render('challenges', 
-    { title: 'Open Challenges', banner: 'banner_challenges.png' }
+    { title: 'Open Challenges', 
+      banner: 'banner_challenges.png',
+      menu: menus(req.route.path)
+    }
   )
 });
 
 app.get('/leaderboard', function(req, res) {
   res.render('leaderboard', 
-    { title: 'Leaderboard', banner: 'banner_leaderboard.png' }
+    { title: 'Leaderboard', 
+      banner: 'banner_leaderboard.png',
+      menu: menus(req.route.path)
+    }
   )
 });
 
